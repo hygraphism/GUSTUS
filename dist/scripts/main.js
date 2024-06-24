1f1138cbd1b71558cea8e39454cb4a165bfc52cc
hygraphism <hygraphism@gmail.com>
2024-06-24 18:48:52 +0200

    nav desktop

---
+++ dist/scripts/main.js
@@ -0,0 +1,7 @@
+const menu =  document.querySelector('.mainMenu')
+const burgerButton = document.querySelector('.hamburger')
+
+burgerButton.addEventListener('click', function(){
+  menu.classList.toggle('isVisible')
+  burgerButton.classList.toggle('is-active')
+})
