21aca6a198569c8d5979fcd05e7822d4f78fc885
hygraphism <hygraphism@gmail.com>
2024-06-25 10:02:55 +0200

    update

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -6,3 +6,47 @@ burgerButton.addEventListener('click', function(){
   menu.classList.toggle('isVisible')
   burgerButton.classList.toggle('is-active')
 })
+
+/* hero animation hover*/
+/////spoon
+let spoon_animation = document.querySelector('.hero_spoon_artifice_content');
+let blockSpoon = document.getElementById('spoonHero'); 
+
+let spoonPlay = lottie.loadAnimation({
+    container: spoon_animation,
+    renderer: 'svg',
+    loop: true,
+    autoplay: false,
+    path: "../dist/lotti/spoon_artifice.json"
+});
+
+blockSpoon.addEventListener('mouseenter', function() {
+  spoonPlay.play();
+});
+
+blockSpoon.addEventListener('mouseleave', function() {
+  spoonPlay.pause();
+});
+
+
+/*
+/////tupp
+let icon_spotify_div = document.querySelector('.icon_spotify_play');
+
+let animationiconplay = lottie.loadAnimation({
+    container: icon_spotify_div,
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    prerender: true,
+    path: "dist/images/button/bouton_musique/musique.json"
+});
+
+let clicked3 = false;
+
+icon_spotify_div.addEventListener('mouseenter', function() {
+    let curFrame = animationiconplay.currentFrame;
+    if(clicked3 == false){
+        animationiconplay.playSegments([0,curFrame],true);
+    }
+})*/
